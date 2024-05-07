ba269a8f334ee2cec8672896eb8f79fa65010e72
Luan <luan.sena1507@gmail.com>
2024-05-07 15:23:47 -0300

    Implementing possible moves of Rook

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -12,14 +12,14 @@ public abstract class Piece {
         return board;
     }
 
-    public abstract boolean[][] PossibleMoves();
+    public abstract boolean[][] possibleMoves();
 
-    public boolean PossibleMove(Position position) { //retorna se pode mover essa peça dada uma posição.
-        return PossibleMoves()[position.getRow()][position.getColumn()];
+    public boolean possibleMove(Position position) { //retorna se pode mover essa peça dada uma posição.
+        return possibleMoves()[position.getRow()][position.getColumn()];
     }
 
     public boolean isThereAnyPossibleMove(){ //Retorna se a peça tem pelo menos algum movimento disponivel
-        boolean[][] mat = PossibleMoves();
+        boolean[][] mat = possibleMoves();
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat.length; j++) {
                 if (mat[i][j]){
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -15,7 +15,7 @@ public class King extends ChessPiece {
     }
 
     @Override
-    public boolean[][] PossibleMoves() {
+    public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
         return mat;
     }
